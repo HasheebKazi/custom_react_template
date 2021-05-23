aeafe32d68fe900d10bf4d3c48961db50ae4867b
Hasheeb <hasheeb.k@gmail.com>
2021-05-23 19:14:05 -0400

    template setup 5/8 - configure eslint and eslint plugins for react and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import left_arrow from './left_arrow.svg'
 import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
+    const sd = 'dog'
+
     return <>
         <h1>React TypeScript Webpack Starter Template {process.env.name} </h1>
-        <img src={shiba_image} alt="Shiba Image" width="300" height="200" />
-        <img src={left_arrow} alt="Shiba Image" width="300" height="200" />
+        <img src={shiba_image} alt="Shiba Inu Dog" width="300" height="200" />
+        <img src={left_arrow} alt="Shiba Inu Dog" width="300" height="200" />
         <ClickCounter />
     </>
 }
