9313a910f11dfab558d80ae190874e6936f0d914
Hasheeb <hasheeb.k@gmail.com>
2021-05-23 19:28:13 -0400

    template setup 6/8 - add prettier and configure prettier with eslint

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
         'plugin:jsx-a11y/recommended',
         // 'plugin:eslint-comments/recommended',
         // 'prettier/@typescript-eslint',
-        // 'plugin:prettier/recommended'
+        'prettier',
+        'plugin:prettier/recommended'
     ],
     rules: {
         'no-unused-vars': 'off',
@@ -30,7 +31,7 @@ module.exports = {
         '@typescript-eslint/no-var-requires': 'off',
         'react/prop-types': 'off',
         // suppress errors for missing 'import React' in files
-        "react/react-in-jsx-scope": "off",
+        'react/react-in-jsx-scope': 'off'
         // allow jsx syntax in js files (for next.js project)
         // "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", "ts", "tsx"] }], //should add ".ts" if typescript project
     }
--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,26 @@ import left_arrow from './left_arrow.svg'
 import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-    const sd = 'dog'
+    const x = 2
 
-    return <>
-        <h1>React TypeScript Webpack Starter Template {process.env.name} </h1>
-        <img src={shiba_image} alt="Shiba Inu Dog" width="300" height="200" />
-        <img src={left_arrow} alt="Shiba Inu Dog" width="300" height="200" />
-        <ClickCounter />
-    </>
+    return (
+        <>
+            <h1>
+                React TypeScript Webpack Starter Template {process.env.name}{' '}
+            </h1>
+            <img
+                src={shiba_image}
+                alt="Shiba Inu Dog"
+                width="300"
+                height="200"
+            />
+            <img
+                src={left_arrow}
+                alt="Shiba Inu Dog"
+                width="300"
+                height="200"
+            />
+            <ClickCounter />
+        </>
+    )
 }
