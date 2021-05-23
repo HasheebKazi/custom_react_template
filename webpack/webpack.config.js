dd9a15cf7b890703baf4cc99688eda2637f0f336
Hasheeb <hasheeb.k@gmail.com>
2021-05-23 18:46:43 -0400

    template setup 3/8 webpack config for prod and dev

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,38 +1,7 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common.js')
 
-module.exports = {
-    entry: path.resolve(__dirname, '..', './src/index.tsx'),
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: require.resolve('babel-loader')
-                }]
-            }, {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader']
-            }, {
-                test: /\.(?:ico|png|jpe?g|gif)$/i,
-                type: 'asset/resource'
-            }, {
-                test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
-                type: 'asset/inline'
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, '..', './build'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    plugins: [
-        new HtmlWebpackPlugin({ template: path.resolve(__dirname, '..', './src/index.html') })
-    ]
+module.exports = ({ env = dev }) => {
+    const envConfig = require(`./webpack.${env}.js`)
+    return merge(commonConfig, envConfig)
 }
