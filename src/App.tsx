035462b4794f8e75cfe3d647d4df352ccb8f0662
Hasheeb <hasheeb.k@gmail.com>
2021-05-23 18:55:27 -0400

    template setup 4/8 added support for react-refresh for live reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css'
 import shiba_image from './shiba_icon.png'
 import left_arrow from './left_arrow.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
     return <>
-        <h1>React TypeScript Webpack Starter Template</h1>
+        <h1>React TypeScript Webpack Starter Template {process.env.name} </h1>
         <img src={shiba_image} alt="Shiba Image" width="300" height="200" />
         <img src={left_arrow} alt="Shiba Image" width="300" height="200" />
+        <ClickCounter />
     </>
 }
